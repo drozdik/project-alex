365abeddc5740a0bfebcc07c6c4eadcad30f5788
Vladimir Drozdik <vladimir.drozdik@gmail.com>
2022-07-17 12:01:59 +0300

    Update componentns without foget. Increase font size.

--- gameui.py
+++ gameui.py
@@ -1,3 +1,4 @@
+from cgitb import text
 import tkinter
 from tkinter import *
 
@@ -37,8 +38,7 @@ class Screen:
         log_content = ""
         for log in self.game_state.get("game_log"):
             log_content += log + "\n"
-
-        self.log_panel = ScrolledText(self.frame, width=50,  height=10)
+        self.log_panel = ScrolledText(self.frame, width=80,  height=15)
         self.log_panel.grid(row=4, column=0, columnspan=3)
         self.log_panel.insert(INSERT, log_content)
         self.log_panel.configure(state ='disabled')
@@ -46,11 +46,11 @@ class Screen:
 
     def on_hero_attack(self):
         self.hero_attack()  # calls action from the Game
-        self.redraw_panels()
+        self.update_components()
 
     def on_hero_heal(self):
         self.use_heal()
-        self.redraw_panels()
+        self.update_components()
 
     def quit(self):
         self.root.destroy()
@@ -65,26 +65,32 @@ class Screen:
         self.frame = tkinter.Frame(self.root)
         self.frame.grid()
         # redraw dynamic elements (log, health)
-        self.redraw_panels(True)
+        self.redraw_game_components()
+
+    def redraw_game_components(self):
+        self.draw_images_and_buttons()
+        self.draw_health_panels()
+        self.log_panel = None
+        self.draw_log_panel()
 
     # images and buttons
     def draw_images_and_buttons(self):
-        knight_image_label = Label(self.frame, image=self.knight_image)
-        knight_image_label.grid(row=0, column=0, columnspan=1)
+        self.knight_image_label = Label(self.frame, image=self.knight_image)
+        self.knight_image_label.grid(row=0, column=0, columnspan=1)
 
         monster1_image = self.skeleton_image
         if self.game_state["active_monster_pack"][0]["name"] == "Skeleton-mage":
             print("monster is mage")
             monster1_image = self.skeleton_mage_image
-        monster1_image_label = Label(self.frame, image=monster1_image)
-        monster1_image_label.grid(row=0, column=1, columnspan=1)
+        self.monster1_image_label = Label(self.frame, image=monster1_image)
+        self.monster1_image_label.grid(row=0, column=1, columnspan=1)
 
         monster2_image = self.skeleton_image
         if self.game_state["active_monster_pack"][1]["name"] == "Skeleton-mage":
             print("monster is mage")
             monster2_image = self.skeleton_mage_image
-        monster2_image_label = Label(self.frame, image=monster2_image)
-        monster2_image_label.grid(row=0, column=2, columnspan=1)
+        self.monster2_image_label = Label(self.frame, image=monster2_image)
+        self.monster2_image_label.grid(row=0, column=2, columnspan=1)
 
         hero_attack_button = Button(self.frame, text="Attack and finish turn", command=self.on_hero_attack)
         hero_attack_button.grid(row=2, column=0, columnspan=1)
@@ -105,9 +111,7 @@ class Screen:
         self.frame = tkinter.Frame(self.root)
         self.frame.grid()
 
-    def redraw_panels(self, with_static=True):
-        if with_static:
-            self.draw_images_and_buttons()
+    def update_components(self):
         if self.game_state.get("hero_dead"):
             self.destroy_frame()
             self.draw_game_over_components("Hero is dead. Game Over.")
@@ -118,27 +122,58 @@ class Screen:
             return
 
         # forget health panels
-        self.hero_health_label.grid_forget()
-        self.monster_1_health_label.grid_forget()
-        self.monster_2_health_label.grid_forget()
-        self.draw_health_panels()
+        # update hero panel
+        self.hero_health_label.config(text=f"Class: Knight\nHealth: {self.game_state.get('hero_hp')}/{self.game_state.get('hero_max_hp')}",
+                                font=("Arial", 14))
+        # update monster1 panel
+        monster1 = self.game_state['active_monster_pack'][0] # it's dictionary now
+        monster1_name = monster1["name"]
+        monster1_hp = monster1["hp"]
+        self.monster_1_health_label.config(text=f"{monster1_name} health: {monster1_hp}",
+                                        font=("Arial", 14))
+        # update monster2 panel
+        monster2 = self.game_state['active_monster_pack'][1] # it's dictionary now
+        monster2_name = monster2["name"]
+        monster2_hp = monster2["hp"]
+        self.monster_2_health_label.config(text=f"{monster2_name} health: {monster2_hp}"
+                                        ,font=("Arial", 14))
+        # update log panel
+        log_content = ""
+        for log in self.game_state.get("game_log"):
+            log_content += log + "\n"
+        self.log_panel.configure(state ='normal')
+        self.log_panel.insert(INSERT, log_content)
+        self.log_panel.configure(state ='disabled')
+        self.log_panel.see("end")
+        # update monster images
+        monster1_image = self.skeleton_image
+        if self.game_state["active_monster_pack"][0]["name"] == "Skeleton-mage":
+            print("monster is mage")
+            monster1_image = self.skeleton_mage_image
+        self.monster1_image_label.config(image=monster1_image)
 
-        self.log_panel.grid_forget()
-        self.draw_log_panel()
+        monster2_image = self.skeleton_image
+        if self.game_state["active_monster_pack"][1]["name"] == "Skeleton-mage":
+            print("monster is mage")
+            monster2_image = self.skeleton_mage_image
+        self.monster2_image_label.config(image=monster2_image)
 
     def draw_health_panels(self):
         self.hero_health_label = Label(self.frame,
-                                text=f"Class: Knight\nHealth: {self.game_state.get('hero_hp')}/{self.game_state.get('hero_max_hp')}")
+                                text=f"Class: Knight\nHealth: {self.game_state.get('hero_hp')}/{self.game_state.get('hero_max_hp')}",
+                                font=("Arial", 14))
         self.hero_health_label.grid(row=1, column=0)
 
         monster1 = self.game_state['active_monster_pack'][0] # it's dictionary now
         monster1_name = monster1["name"]
         monster1_hp = monster1["hp"]
-        self.monster_1_health_label = Label(self.frame, text=f"{monster1_name} health: {monster1_hp}")
+        self.monster_1_health_label = Label(self.frame, text=f"{monster1_name} health: {monster1_hp}",
+                                        font=("Arial", 14))
         self.monster_1_health_label.grid(row=1, column=1)
 
         monster2 = self.game_state['active_monster_pack'][1] # it's dictionary now
         monster2_name = monster2["name"]
         monster2_hp = monster2["hp"]
-        self.monster_2_health_label = Label(self.frame, text=f"{monster2_name} health: {monster2_hp}")
+        self.monster_2_health_label = Label(self.frame, text=f"{monster2_name} health: {monster2_hp}"
+                                        ,font=("Arial", 14))
         self.monster_2_health_label.grid(row=1, column=2)

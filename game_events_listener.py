3304923c86c507382c12a13c48835bd967c0f140
Vladimir Drozdik <vladimir.drozdik@gmail.com>
2022-08-29 23:19:01 +0300

    Fix Skeleton test using TestGameEventsListener

--- game_events_listener.py
+++ game_events_listener.py
@@ -7,3 +7,11 @@ class GameEventsListener:
             self.screenHolder["screen"].on_hero_armor_changed(-2)
         else:
             raise Exception("Unknown type of event", event)
+
+
+class TestGameEventsListener:
+    def __init__(self):
+        self.events = []
+
+    def push_event(self, event):
+        self.events.append(event)
--- test_skeleton.py
+++ test_skeleton.py
@@ -1,5 +1,6 @@
 import unittest
 
+from game_events_listener import TestGameEventsListener
 from monsters import Skeleton
 
 
@@ -33,7 +34,8 @@ class TestUser(unittest.TestCase):
         initial_hero_armor = 10
         game_state = new_game_state()
         game_state["hero_armor"] = initial_hero_armor
-        skeleton = Skeleton(self.screen_holder)
+        events_listener = TestGameEventsListener()
+        skeleton = Skeleton(self.screen_holder, events_listener)
 
         # when
         skeleton.special(game_state)
@@ -43,7 +45,8 @@ class TestUser(unittest.TestCase):
         self.assertEqual(game_state["hero_armor"], initial_hero_armor - 2, message)
 
     def test_skeleton_hp_same_when_damage_less_or_equal_to_armor(self):
-        skeleton = Skeleton(self.screen_holder)
+        events_listener = TestGameEventsListener()
+        skeleton = Skeleton(self.screen_holder, events_listener)
         initial_hp = skeleton.hp
         skeleton.take_damage(skeleton.armor - 1)
         skeleton.take_damage(skeleton.armor)
